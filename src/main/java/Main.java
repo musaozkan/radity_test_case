9dfee2224eb0472528713992997aa2e392ce5c27
ozkanmus <musa.ozkan@commencis.com>
2025-05-17 01:18:06 +0300

    VelocityRenderer is implemented and .vm template created with relevant Main.java modifications

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import model.Client;
 import model.Invoice;
+import service.VelocityRenderer;
 
 import java.net.URL;
 import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,10 +18,20 @@ public class Main {
                     new URL("http://localhost:3000/invoices"),
                     new TypeReference<List<Invoice>>() {}
             );
+            List<Client> clients = mapper.readValue(
+                    new URL("http://localhost:3000/clients"),
+                    new TypeReference<List<Client>>() {}
+            );
             Invoice invoice = invoices.get(0); // Take the first invoice
+            Client client = clients.get(0); // Take the first client
 
-            // Print output to verify
-            System.out.println(invoice.toString());
+            String html = VelocityRenderer.renderInvoice(invoice, client);
+
+            // Save HTML to file
+            Path outputPath = Path.of("outputs", "invoice.html");
+            Files.createDirectories(outputPath.getParent());
+            Files.writeString(outputPath, html);
+            System.out.println("HTML output saved to: " + outputPath.toAbsolutePath());
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/service/VelocityRenderer.java
+++ src/main/java/service/VelocityRenderer.java
@@ -1,4 +1,34 @@
 package service;
 
+import model.*;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+
+import java.io.StringWriter;
+import java.util.Properties;
+
 public class VelocityRenderer {
+
+    public static String renderInvoice(Invoice invoice, Client client) throws Exception {
+        Properties props = new Properties();
+        props.setProperty("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+
+        VelocityEngine velocityEngine = new VelocityEngine(props);
+        velocityEngine.init();
+
+        Template template = velocityEngine.getTemplate("template/invoice_template.vm");
+
+        VelocityContext context = new VelocityContext();
+        context.put("invoice", invoice);
+        context.put("address", invoice.getBillingAddress());
+        context.put("items", invoice.getItems());
+        context.put("client", client);   // And this
+
+        StringWriter writer = new StringWriter();
+        template.merge(context, writer);
+
+        return writer.toString();
+    }
+
 }
